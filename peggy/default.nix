eb432d18b753797aa73ed02e7e8b70b0b62d6516
Sam <30577766+Samasaur1@users.noreply.github.com>
2023-10-27 15:54:39 -0700

    Add Vaughn's user

--- peggy/default.nix
+++ peggy/default.nix
@@ -12,4 +12,9 @@
   };
 
   networking.hostName = "peggy";
+
+  users.users.vzaayer = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMbKaxdOlA1VOD3R7ZGsiHRTKZR7CE+rT+G/4FSh++08 vzaayer" ];
+  };
 }
